f930919219a3aa88681c0171fb11b56e0f39a7f8
yangtuo <yangtuo@raycloud.com>
2020-07-14 10:32:20 +0800

    add test case

--- test/pubsubManage.spec.ts
+++ test/pubsubManage.spec.ts
@@ -23,6 +23,27 @@ it('发布', () => {
     expect(mockCallback).toBeCalledTimes(3)
 })
 
+it('多个订阅者 发布', () => {
+    function cb(value) {
+        return value
+    }
+    const mockCallback = jest.fn(cb)
+    // const mockCallback2 = jest.fn(cb)
+    const pubSub = new PubSubManage()
+    const subscription = pubSub.on(listenKey).subscribe(mockCallback)
+    const subscription1 = pubSub.on(listenKey).subscribe(mockCallback)
+    pubSub.emit(listenKey, { type: listenKey, value: 'test' })
+    pubSub.emit(listenKey)
+    expect(mockCallback.mock.calls[0][0]).toEqual({ type: listenKey, value: 'test' })
+    expect(mockCallback).toBeCalledTimes(4)
+    subscription1.unsubscribe()
+    // subscription.unsubscribe()
+    pubSub.emit(listenKey)
+    expect(mockCallback).toBeCalledTimes(5)
+})
+
+
+
 it('取消订阅', () => {
     const pubSub = new PubSubManage()
     pubSub.on(listenKey)
